1e1fb07502eb3f4ad8207fd4d5cb8d34a84c14ec
John Mutuma <johnmutuma5@gmail.com>
2018-06-05 10:24:43 +0300

    Add function getUser

--- src/functions/functions.js
+++ src/functions/functions.js
@@ -1,6 +1,10 @@
 const functions = {
     add: (num, num1) => num + num1,
     isNull: () => null,
+    getUser: () = ({
+        name: "John",
+        occupation: "Software"
+    })
 }
 
 module.exports = functions;
