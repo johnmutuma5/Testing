0c43e1ddec16c5c2c06af257ac4a86280dba27c0
John Mutuma <johnmutuma5@gmail.com>
2018-06-05 09:34:05 +0300

    Add test for null

--- src/functions/tests/functions.test.js
+++ src/functions/tests/functions.test.js
@@ -4,3 +4,13 @@ const add = require('../functions');
 test("it adds correctly", () => {
     expect(add(2, 2)).toBe(4);
 });
+
+
+test("it does not return 5 for 2+2", () => {
+    expect(add(2, 2)).not.toBe(5);
+});
+
+
+test("it is null", () => {
+    expect(functions.isNull().toBeNull());
+})
